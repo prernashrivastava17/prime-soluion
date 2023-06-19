5e94afff9098c77f6b04ef51829b69b475f25068
root <root@ip-172-31-28-190.eu-north-1.compute.internal>
2023-06-19 18:23:26 +0000

    updated

---
+++ api/config/db.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    HOST: '16.170.163.186',
+    USER: "root",
+    PASSWORD: "vishal1234",
+    DB: "primeSolutionMarket",
+    dialect: "mysql",
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  };
+  
